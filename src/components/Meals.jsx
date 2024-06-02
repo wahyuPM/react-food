7710efd7fa596be36587e321838cad4b082f2b7e
wahyuPM <wahyupratama191@gmail.com>
2024-06-02 20:44:40 +0800

    add: meals component add: context api

---
+++ src/components/Meals.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect, useCallback } from "react";
+import MealItem from "./MealItem";
+
+export default function Meals() {
+    const [loadedMeals, setLoadedMeals] = useState([])
+
+    const fetchMeals = useCallback( // useCallback berfungsi untuk memoize function agar tidak menyebabkan rerender ulang ketika dijadikan dependency pada useEffect
+        async function fetchMeals() {
+            try {
+                const response = await fetch('http://localhost:3000/meals')
+
+                if (!response.ok) {
+                    //..
+                }
+
+                const meals = await response.json()
+                setLoadedMeals(meals)
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        [],
+    )
+
+
+    useEffect(() => {
+        fetchMeals()
+    }, [fetchMeals])
+
+    console.log('rerender');
+
+    return <ul id="meals">
+        {
+            loadedMeals.map((meal) => <MealItem key={meal.id} meal={meal} />)
+        }
+    </ul>
+}
---
+++ src/components/UI/Button.jsx
@@ -0,0 +1,5 @@
+export default function Button({ children, textOnly, className, ...props }) {
+    const cssClasses = textOnly ? `text-button ${className}` : 'button'
+
+    return <button className={cssClasses} {...props}>{children}</button>
+}
