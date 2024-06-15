54e9ec65e51b5e25f1d526e0e18fe11c319ec959
wahyuPM <wahyupratama191@gmail.com>
2024-06-15 22:18:52 +0800

    add : integrate checkout api

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -5,14 +5,32 @@ import { currencyFormatter } from "../utils/formatting";
 import Input from "./UI/Input";
 import Button from "./UI/Button";
 import UserProgressContext from "../store/UserProgressContext";
+import useHttp from "../hooks/useHttp"
+import Error from "../components/Error"
+
+const requestConfig = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+}
 
 export default function Checkout() {
     const cartCtx = useContext(CartContext)
     const userProgressCtx = useContext(UserProgressContext)
+
+    const { data, isLoading, error, sendRequest, clearData } = useHttp('http://localhost:3000/orders', requestConfig)
+
     const cartTotal = cartCtx.items.reduce((totalPrice, item) => totalPrice + item.quantity * item.price, 0)
 
     function handleClose() {
         userProgressCtx.hideCheckout()
+    }
+
+    function handleFinish() {
+        userProgressCtx.hideCheckout()
+        cartCtx.clearCart()
+        clearData()
     }
 
     function handleSubmit(event) {
@@ -21,18 +39,32 @@ export default function Checkout() {
         const fd = new FormData(event.target)
         const customerData = Object.fromEntries(fd.entries())
 
-        fetch('http://localhost:3000/orders', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                order: {
-                    items: cartCtx.items,
-                    customer: customerData
-                }
-            })
-        })
+        sendRequest(JSON.stringify({
+            order: {
+                items: cartCtx.items,
+                customer: customerData
+            }
+        }))
+    }
+
+    let actions = <>
+        <Button onClick={handleClose} type="button" textOnly>Close</Button>
+        <Button>Submit Order</Button>
+    </>
+
+    if (isLoading) {
+        actions = <span>Sending order data...</span>
+    }
+
+    if (data && !error) {
+        return <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleFinish}>
+            <h2>Success!</h2>
+            <p>Your order was submitted successfully</p>
+            <p>We will get back to you with more details via email within the next few minutes.</p>
+            <p className="modal-actions">
+                <Button onClick={handleFinish}>Okay</Button>
+            </p>
+        </Modal>
     }
 
     return <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose}>
@@ -47,9 +79,11 @@ export default function Checkout() {
                 <Input label="Postal Code" type="text" id="postal-code" />
                 <Input label="City" type="text" id="city" />
             </div>
+
+            {error && <Error title="failed to submit order" message={error} />}
+
             <p className="modal-actions">
-                <Button onClick={handleClose} type="button" textOnly>Close</Button>
-                <Button>Submit Order</Button>
+                {actions}
             </p>
         </form>
     </Modal>
--- src/hooks/useHttp.js
+++ src/hooks/useHttp.js
@@ -17,10 +17,14 @@ export default function useHttp(url, config, initialData) {
     const [isLoading, setIsLoading] = useState(false)
     const [error, setError] = useState()
 
-    const sendRequest =  useCallback(async function sendRequest(){
+    function clearData() {
+        setData(initialData)
+    }
+
+    const sendRequest =  useCallback(async function sendRequest(data){
         setIsLoading(true)
         try {
-            const resData = await sendHttpRequest(url, config)
+            const resData = await sendHttpRequest(url, {...config, body: data})
             setData(resData)
         } catch (error) {
             setError(error.message || 'Something went wrong!')
@@ -35,6 +39,6 @@ export default function useHttp(url, config, initialData) {
     }, [sendRequest, config])
 
     return {
-        data, isLoading, error, sendRequest
+        data, isLoading, error, sendRequest, clearData
     }
 }
